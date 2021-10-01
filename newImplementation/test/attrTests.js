02babec73dcd676cd6b9977d75d2f99c123175ab
Marcelo Cesar da Silva <marcelo.cesards@gmail.com>
2021-10-01 18:35:48 -0300

    adicionando uma class em uma div

--- newImplementation/test/attrTests.js
+++ newImplementation/test/attrTests.js
@@ -14,3 +14,7 @@ assert.assert(jsml.toHtml({"html":{"lang":"pt-BR"}}), '<html lang="pt-BR"></html
  * DOCTYPE tag with html attribute
 */
 assert.assert(jsml.toHtml({"!DOCTYPE html":null}), '<!DOCTYPE html>', "!DOCTYPE tag with html attr");
+/**
+ * div with class
+*/
+assert.assert(jsml.toHtml({"div":{"class":"btn btn-danger"}}), '<div class="btn btn-danger"></div>', "div with class attribute");
