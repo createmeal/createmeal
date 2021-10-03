a88d83e5956a7e1028645bc4c47339cd2fd92d76
Marcelo Cesar da Silva <marcelo.cesards@gmail.com>
2021-10-03 00:32:15 -0300

    complex json test

--- newImplementation/test/estruturaComplexa.js
+++ newImplementation/test/estruturaComplexa.js
@@ -12,12 +12,75 @@ const json = {
     "html": {
     }
 }
-assert.assert(jsml.toHtml(json), 'teste', "basic string array node");
+assert.assert(jsml.toHtml(json), '<!DOCTYPE html><html></html>', "01-doctype with attribute and html");
 
+var t ={
+    "div": [//<div class="container">
+        {"class": "container"},
+        {"h1": {//<h1 content="Teste de criacao - FAILED"></h1>
+            "content": "Teste de criacao - FAILED"
+        }},
+        {"h2":["passou!!!"]},//<h2>passou!!!</h2>
+        {"div": {//<div></div>
+        }},
+        {
+            "ul": [//<ul>
+                    {
+                        "li": [//<li class="list-group-item">Cras justo odio</li></ul>`;
+                            {"class": "list-group-item"},
+                            "Cras justo odio"
+                        ]
+                    },
+                    {
+                        "li": [//<li class="list-group-item">Dapibus ac facilisis in</li>
+                            {"class": "list-group-item"},
+                            "Dapibus ac facilisis in"
+                        ]
+                    },
+                    {
+                        "li": [//<li class="list-group-item">Morbi leo risus</li>
+                            {"class": "list-group-item"},
+                            "Morbi leo risus"
+                        ]
+                    },
+                    {
+                        "li": [//<li class="list-group-item">Porta ac consectetur ac</li>
+                            {"class": "list-group-item"},
+                            "Porta ac consectetur ac"
+                        ]
+                    },
+                    {
+                        "li": [//<li class="list-group-item">Vestibulum at eros</li>
+                            {"class": "list-group-item"},
+                            "Vestibulum at eros"
+                        ]
+                    }
+                ]
+        },
+        {//<script></script><script></script><script></script></div>
+            "script": {
+                
+            }
+        },
+        {
+            "script": {
+            
+            }
+        },
+        {
+            "script": {
+            
+            }
+        }
+    
+    ]
+}
+var result = `<div class="container"><h1 content="Teste de criacao - FAILED"></h1><h2>passou!!!</h2><div></div><ul><li class="list-group-item">Cras justo odio</li><li class="list-group-item">Dapibus ac facilisis in</li><li class="list-group-item">Morbi leo risus</li><li class="list-group-item">Porta ac consectetur ac</li><li class="list-group-item">Vestibulum at eros</li></ul><script></script><script></script><script></script></div>`;
+assert.assert(jsml.toHtml(t), result, "02-complex extructure");
 
-const noAttr = {
+noAttr = {
     "!DOCTYPE": {
-    },
+    },    
     "html": {
         "head": {
             "link": {
@@ -29,8 +92,9 @@ const noAttr = {
             "div": {
                 "class": "container",
                 "h1": {
-                    "content": "Teste de criacao"
+                    "content": "Teste de criacao - FAILED"
                 },
+                "h2":["passou!!!"],
                 "div": {
                 },
                 "children": [
@@ -90,4 +154,5 @@ const noAttr = {
         }
     }
 }
-assert.assert(jsml.toHtml(noAttr), 'teste', "02-no attr");
+result = `<!DOCTYPE><html><head><link></link><title></title><head><body><div class="container"><h1 content="Teste de criacao - FAILED"></h1><h2>passou!!!</h2><div></div><ul><li class="list-group-item">Cras justo odio</li><li class="list-group-item">Dapibus ac facilisis in</li><li class="list-group-item">Morbi leo risus</li><li class="list-group-item">Porta ac consectetur ac</li><li class="list-group-item">Vestibulum at eros</li></ul><script></script><script></script><script></script></div></body></html>`;
+assert.assert(jsml.toHtml(noAttr), result, "03-complex json with new ideas");
