d896b609c90a489a1921aa37071cce8cbea73de7
Marcelo Cesar da Silva <marcelo.cesards@gmail.com>
2021-10-13 23:39:22 -0300

    creating about the project

--- README.html
+++ README.html
@@ -37,5 +37,95 @@
                 >
             </p>
         </div>
+        <details>
+            <summary>Table of Contents</summary>
+            <ol>
+                <li>
+                    <a href="#about-the-project"
+                        ><span>About The Project</span></a
+                    >
+                    <ul>
+                        <li>
+                            <a href="#built-with"><span>Built With</span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#getting-started"><span>Getting Started</span></a>
+                    <ul>
+                        <li>
+                            <a href="#prerequisites"
+                                ><span>Prerequisites</span></a
+                            >
+                        </li>
+                        <li>
+                            <a href="#installation"
+                                ><span>Installation</span></a
+                            >
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#usage"><span>Usage</span></a>
+                </li>
+                <li>
+                    <a href="#roadmap"><span>Roadmap</span></a>
+                </li>
+                <li>
+                    <a href="#contributing"><span>Contributing</span></a>
+                </li>
+                <li>
+                    <a href="#license"><span>License</span></a>
+                </li>
+                <li>
+                    <a href="#contact"><span>Contact</span></a>
+                </li>
+                <li>
+                    <a href="#acknowledgments"><span>Acknowledgments</span></a>
+                </li>
+            </ol>
+        </details>
+        <section>
+            <h2>About The Project</h2>
+            <p>
+                HTML is the standard way to produce web page content and there
+                are several other options to produce text estructures like
+                markdown, jsml, WackoWiki Markup, AsciiDoc, Contentflow, and
+                others. But to work with these alternatives at first you will
+                need learn a new language, is not ease to produce content using
+                javascript, and most of them has a strange sintax and some times
+                worst then HTML.
+            </p>
+            <p>
+                Createmeal gives use the same HTML tag names and semantic,
+                butwith json as sintax. It decreases the need of learn a new
+                language, gives you a better way to generate layout content in a
+                better way using javascript, in additional to allow you storage
+                source easilly in database if is needed, componentize, create
+                templates and serialize to object.
+            </p>
+            <p>
+                Of course, for many people, HTML is excelent, but is great when
+                you have good alternatives to solve problems, and turn live
+                better.
+            </p>
+        </section>
+        <section>
+            <h3>Built With</h3>
+            <p>
+                The entire library is actually created using javascript and the
+                dependences and HTML is used only for Development and testing,
+                and example porpose
+            </p>
+        </section>
+        <h2>Getting Started</h2>
+        <h3>Prerequisites</h3>
+        <h3>Installation</h3>
+        <h2>Usage</h2>
+        <h2>Roadmap</h2>
+        <h2>Contributing</h2>
+        <h2>License</h2>
+        <h2>Contact</h2>
+        <h2>Acknowledgments</h2>
     </body>
 </html>
--- documentation/readme-generator.js
+++ documentation/readme-generator.js
@@ -2,8 +2,10 @@ const createmeal = require("../index");
 const fs = require("fs");
 const path = require("path");
 const ISSUES_URL = "https://github.com/createmeal/createmeal/issues";
-const CONTRIBUTORS_URL = "https://github.com/createmeal/createmeal/graphs/contributors";
-const LICENSE_URL = "https://github.com/createmeal/createmeal/blob/master/LICENSE";
+const CONTRIBUTORS_URL =
+    "https://github.com/createmeal/createmeal/graphs/contributors";
+const LICENSE_URL =
+    "https://github.com/createmeal/createmeal/blob/master/LICENSE";
 const DOCS_URL = "#";
 
 let readme = {
@@ -11,32 +13,127 @@ let readme = {
     html: {
         body: [
             { div: { id: "top" } },
-            createBedge(CONTRIBUTORS_URL, "CONTRIBUTORS",  2,  "brightgreen"),
-            {span: [" "]},
-            createBedge(ISSUES_URL, "ISSUES",  "0", "yellow"),
-            {span: [" "]},
+            createBedge(CONTRIBUTORS_URL, "CONTRIBUTORS", 2, "brightgreen"),
+            { span: [" "] },
+            createBedge(ISSUES_URL, "ISSUES", "0", "yellow"),
+            { span: [" "] },
             createBedge(LICENSE_URL, "LICENSE", "MIT", "green"),
-            { div: [
-                { align: "center" },
-                {h3: ["Createmeal"]},
-                {
-                    p: [
-                        { align: "center" },
-                        {span: ["HTML generator powered by json"]},
-           
-                        {a: {style:"display:block", href: DOCS_URL,  strong: ["Explore the docs »"]}}
-                    ]                    
-                },
-                {
-                    p: [
-                        {a: { href: "#",  span: ["View Demo"]}},
-                        {span: [" · "]},
-                        {a: { href: ISSUES_URL,  span: ["Report Bug"]}},
-                        {span: [" · "]},
-                        {a: { href: ISSUES_URL,  span: ["Request Feature"]}}
-                    ]
-                }
-            ] },
+            {
+                div: [
+                    { align: "center" },
+                    { h3: ["Createmeal"] },
+                    {
+                        p: [
+                            { align: "center" },
+                            { span: ["HTML generator powered by json"] },
+
+                            {
+                                a: {
+                                    style: "display:block",
+                                    href: DOCS_URL,
+                                    strong: ["Explore the docs »"],
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        p: [
+                            { a: { href: "#", span: ["View Demo"] } },
+                            { span: [" · "] },
+                            { a: { href: ISSUES_URL, span: ["Report Bug"] } },
+                            { span: [" · "] },
+                            {
+                                a: {
+                                    href: ISSUES_URL,
+                                    span: ["Request Feature"],
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                details: [
+                    { summary: ["Table of Contents"] },
+                    {
+                        ol:[
+                            {li:{
+                                a: {
+                                    href: "#about-the-project",
+                                    span: ["About The Project"],
+                                },
+                                ul: createTableOfContentsLine("#built-with", "Built With"),
+                            }},
+                            {li:{
+                                a: {
+                                    href: "#getting-started",
+                                    span: ["Getting Started"],
+                                },
+                                ul: [
+                                    createTableOfContentsLine("#prerequisites", "Prerequisites"),
+                                    createTableOfContentsLine("#installation", "Installation")
+                                ]
+                            }},
+                            createTableOfContentsLine("#usage", "Usage"),
+                            createTableOfContentsLine("#roadmap", "Roadmap"),
+                            createTableOfContentsLine("#contributing", "Contributing"),
+                            createTableOfContentsLine("#license", "License"),
+                            createTableOfContentsLine("#contact", "Contact"),
+                            createTableOfContentsLine("#acknowledgments", "Acknowledgments")                            
+                        ]
+                    }
+                    
+                ],
+            },
+            {
+                section:[
+                    {h2:["About The Project"]},
+                    {p:[
+                        "HTML is the standard way to produce web page content ",
+                        "and there are several other options to produce text estructures like markdown, ",
+                        "jsml,  WackoWiki Markup, AsciiDoc, Contentflow, and others. But to ",
+                        " work with these alternatives at first you will need learn a new language, ",
+                        "is not ease to produce content using javascript, and most of them has a ",
+                        "strange sintax and some times worst then HTML."
+                    ]},
+                    {p:[
+                        "Createmeal gives use the same HTML tag names and semantic, but",
+                        "with json as sintax. It decreases the need of learn a new language, ",
+                        "gives you a better way to generate layout content in a better way using ",
+                        "javascript, in additional to allow you storage source easilly in database ", 
+                        "if is needed, componentize, create templates and serialize to object."
+                    ]},
+                    {p:[
+                        "Of course, for many people, HTML is excelent, but is great when you have ",
+                        "good alternatives to solve problems, and turn live better."
+                    ]},
+                    {p:[
+                        {align:"right"},
+                        createSimpleLink("#top", "back to top")
+                    ]}
+            ]},
+            {
+                section:[
+                    {h3:["Built With"]},
+                    {
+                        p:[
+                            "The entire library is actually created using javascript and ",
+                            "the dependences and HTML is used only for Development and ",
+                            " testing, and example porpose."
+                        ]
+                    }
+                ]
+            },
+            
+            {h2:["Getting Started"]},
+            {h3:["Prerequisites"]},
+            {h3:["Installation"]},
+            {h2:["Usage"]},
+            {h2:["Roadmap"]},
+            {h2:["Contributing"]},
+            {h2:["License"]},
+            {h2:["Contact"]},
+            {h2:["Acknowledgments"]}
         ],
     },
 };
@@ -54,4 +151,19 @@ function createBedge(link, label, message, color) {
             },
         },
     };
+}
+
+function createTableOfContentsLine(href, textContent) {
+    return {
+        li:createSimpleLink(href, textContent)
+    }    
+}
+
+function createSimpleLink(href, textContent){
+    return {
+        a: {
+            href: href,
+            span: [textContent],
+        }
+    }
 }
