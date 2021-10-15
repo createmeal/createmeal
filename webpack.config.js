61a18701a8e80677d965ffca9a8d1e5dd729c3d2
Julio Cezar da Silva <jcezardasilva@outlook.com>
2021-10-15 16:45:32 -0300

    atualização de configuração webpack e consumo em exemplos

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
+    target: "web",
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'createmeal.js',
         library: {
             name: "createmeal",
-            type: "umd"
-        }
+            type: "umd",
+            umdNamedDefine: true
+        },
+        globalObject: "this"
     }
 }
