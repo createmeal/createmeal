88580fa2626f50a6ebbb1d05a0127bbcc8bd79fd
Julio Cezar da Silva <jcezardasilva@outlook.com>
2022-01-19 16:14:04 -0300

    atualização de testes de commonjs para ES modules

--- test/childTest.js
+++ test/childTest.js
@@ -1,34 +1,34 @@
-const jsml = require('../index')
-const assert = require('./assert')
+import createmeal from '../index.js';
+import assert from './assert.js';
 
  /**
   * Html with children
   */
- assert.assert(jsml.toHtml({"html":{"div":{}}}), "<html><div></div></html>", "01-html with div child tag");
+ assert.assert(createmeal.toHtml({"html":{"div":{}}}), "<html><div></div></html>", "01-html with div child tag");
 
   /**
   * Html with children
   */
-   assert.assert(jsml.toHtml({"html":{"head":{}}}), "<html><head></head></html>", "02-html with head child tag");
+   assert.assert(createmeal.toHtml({"html":{"head":{}}}), "<html><head></head></html>", "02-html with head child tag");
 
    /**
   * Html with children
   */
-    assert.assert(jsml.toHtml({"html":{"head":{}, "body":{}}}), "<html><head></head><body></body></html>", "03-html with head and body children");
+    assert.assert(createmeal.toHtml({"html":{"head":{}, "body":{}}}), "<html><head></head><body></body></html>", "03-html with head and body children");
   
   /**
   * body with children
   */
-var input = {"html":{"head":{}, "body":{"div":{}}}};
-var output = "<html><head></head><body><div></div></body></html>";
-assert.assert(jsml.toHtml(input), output, "04-html with head, body and body children");
+let input = {"html":{"head":{}, "body":{"div":{}}}};
+let output = "<html><head></head><body><div></div></body></html>";
+assert.assert(createmeal.toHtml(input), output, "04-html with head, body and body children");
 
   /**
   * doctype, html and body with children
   */
    input = [{"!DOCTYPE html":""},{"html":{"head":{}, "body":{"div":{}}}}];
    output = "<!DOCTYPE html><html><head></head><body><div></div></body></html>";
-   assert.assert(jsml.toHtml(input), output, "05-doctype, html with head, body and body children");
+   assert.assert(createmeal.toHtml(input), output, "05-doctype, html with head, body and body children");
 
  /**
   * doctype, html and body with children
@@ -42,7 +42,7 @@ assert.assert(jsml.toHtml(input), output, "04-html with head, body and body chil
     }
   }];
   output = "<html><head><link><title></title></head></html>";
-  assert.assert(jsml.toHtml(input), output, "06-html with head, and head with link and title");
+  assert.assert(createmeal.toHtml(input), output, "06-html with head, and head with link and title");
 
   /**
   * doctype, html and body with children
@@ -56,7 +56,7 @@ assert.assert(jsml.toHtml(input), output, "04-html with head, body and body chil
     }
   }];
   output = "<html><head><link ref><title></title></head></html>";
-  assert.assert(jsml.toHtml(input), output, "06-html with head, and head with link and title");
+  assert.assert(createmeal.toHtml(input), output, "06-html with head, and head with link and title");
 
   /**
   * body multiple children
@@ -68,4 +68,4 @@ assert.assert(jsml.toHtml(input), output, "04-html with head, body and body chil
       }
   };
   output = '<body class="container"><h1></h1></body>';
-  assert.assert(jsml.toHtml(input), output, "07-body multiple children");
+  assert.assert(createmeal.toHtml(input), output, "07-body multiple children");
