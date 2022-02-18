40d5b391137e8f75081edf0788effcb7051d5129
Marcelo Cesar <marcelo.cesards@gmail.com>
2022-02-18 18:20:37 -0300

    adding test for options

---
+++ test/test_option.js
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import createmeal from '../src/index.js';
+describe('Option', function() {
+  describe('01-custon attribute prefix', function() {
+    it('should return div with an attribute *ngIf', function() {
+        const input = {
+            div: {
+                "*ngIf":"let i == tsField.value"
+            }
+        }
+        const expected=`<div *ngIf="let i == tsField.value"></div>`;
+      assert.equal(createmeal.toHtml(input), expected);
+    });
+  });
+});
