3b29dabc3425e4a15f8400996f7bfa9ad92b83a9
Marcelo Cesar <marcelo.cesards@gmail.com>
2022-02-18 23:26:52 -0300

    refactoring index
    - creating method createNodesFromArray
    -creating method createNodesFromObject

--- src/index.js
+++ src/index.js
@@ -13,26 +13,52 @@ import NodeFactory from "./composite/NodeFactory.js";
 }
 
 /**
- * responsible for iterate all json nodes independing if is object or array.
- * @param {*} jsonDoc 
+ * check main node if it is an object or array
+ * to call right method to create nodes.
+ * @param {[]|{}} jsonDoc 
  */
 function foreEachNode(jsonDoc){
     const nodeFactory = new NodeFactory();
     let nodes = [];
     if(Array.isArray(jsonDoc)){
-        for(let child of jsonDoc){
-            let node = nodeFactory.getNode(child);
-            if(Array.isArray(node))
-                nodes = nodes.concat(node);
-            else
-                nodes.push(node);
-        }
+        nodes = createNodesFromArray(jsonDoc, nodeFactory, nodes);
     } else if(typeof jsonDoc ==="object"){
-        let newnodes = nodeFactory.getNode(jsonDoc);
-        if(Array.isArray(newnodes))
-            nodes = nodes.concat(newnodes);
+        nodes = createNodesFromObject(nodeFactory, jsonDoc, nodes);
+    }
+    return nodes;
+}
+
+/**
+ * Iterates over a jsonDoc array to extract nodes
+ * @param {[]} jsonDoc 
+ * @param {[]} nodes 
+ * @returns nodes
+ */
+function createNodesFromObject(nodeFactory, jsonDoc, nodes) {
+    let newnodes = nodeFactory.getNode(jsonDoc);
+    if (Array.isArray(newnodes))
+        nodes = nodes.concat(newnodes);
+
+    else
+        nodes.push(newnodes);
+    return nodes;
+}
+
+/**
+ * Get children of jsonDoc objec to
+ * create nodes
+ * @param {{}} jsonDoc 
+ * @param {[]} nodes 
+ * @returns nodes
+ */
+function createNodesFromArray(jsonDoc, nodeFactory, nodes) {
+    for (let child of jsonDoc) {
+        let node = nodeFactory.getNode(child);
+        if (Array.isArray(node))
+            nodes = nodes.concat(node);
+
         else
-            nodes.push(newnodes);
+            nodes.push(node);
     }
     return nodes;
 }
