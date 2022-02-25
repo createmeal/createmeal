136fbe096fffd8e6461a4bb02ade6e39e4fb392e
Marcelo Cesar <marcelo.cesards@gmail.com>
2022-02-24 21:25:50 -0300

    tag abbr

--- test/test_tag.js
+++ test/test_tag.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import createmeal from '../src/index.js';
 import tags from '../src/tags.js';
 describe('tag', function() {
-  const testNumber="03";
+  let testNumber="03";
   let tagName = "a";
   describe(`${testNumber} - tag "${tags[tagName].tag}": ${tags[tagName].Description}`, function() {    
     it(`${testNumber}.1 - when value is string, should set textContent`, function() {
@@ -13,4 +13,16 @@ describe('tag', function() {
       assert.equal(createmeal.toHtml(input), expected);
     });   
   });
+  
+  testNumber=4;
+  tagName = "abbr";
+  describe(`${testNumber} - tag "${tags[tagName].tag}": ${tags[tagName].Description}`, function() {    
+    it(`${testNumber}.1 - when {${tagName}:"textContent value"}, should return: <${tagName}>textContent value</${tagName}>`, function() {
+        let input = {}
+        input[tagName] = "textContent value"
+        const expected=`<${tagName}>textContent value</${tagName}>`;
+      assert.equal(createmeal.toHtml(input), expected);
+    });   
+  });
+  
 });
