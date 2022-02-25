a5d576fe19900d943dd10dd6e2254b61a118b541
Marcelo Cesar <marcelo.cesards@gmail.com>
2022-02-24 22:22:05 -0300

    test tag blockquote

--- test/test_tag.js
+++ test/test_tag.js
@@ -177,4 +177,117 @@ describe('tag', function() {
       assert.equal(createmeal.toHtml(input), expected);
     });   
   });
+  
+  describe(`19 - tag "${tags["blockquote"].tag}": ${tags["blockquote"].Description}`, function() {
+    const tagName = "blockquote";   
+    it(`19.1 - when {${tagName}:"textContent value"}, should return: <${tagName}>textContent value</${tagName}>`, function() {
+        let input = {}
+        input[tagName] = "textContent value"
+        const expected=`<${tagName}>textContent value</${tagName}>`;
+      assert.equal(createmeal.toHtml(input), expected);
+    });   
+  });
+  //
+//body
+//br
+//button
+//canvas
+//caption
+//center
+//cite
+//code
+//col
+//colgroup
+//command
+//datagrid
+//datalist
+//dd
+//del
+//details
+//dfn
+//dialog
+//dir
+//div
+//dl
+//dt
+//em
+//embed
+//eventsource
+//fieldset
+//figcaption
+//figure
+//font
+//footer
+//form
+//frame
+//frameset
+//h1
+//head
+//header
+//hgroup
+//hr
+//html
+//i
+//iframe
+//img
+//input
+//ins
+//isindex
+//kbd
+//keygen
+//label
+//legend
+//li
+//link
+//map
+//mark
+//menu
+//meta
+//meter
+//nav
+//noframes
+//noscript
+//object
+//ol
+//optgroup
+//option
+//output
+//p
+//param
+//pre
+//progress
+//q
+//rp
+//rt
+//ruby
+//s
+//samp
+//script
+//section
+//select
+//small
+//source
+//span
+//strike
+//strong
+//style
+//sub
+//sup
+//table
+//tbody
+//td
+//textarea
+//tfoot
+//th
+//thead
+//time
+//title
+//tr
+//track
+//tt
+//u
+//ul
+//var
+//video
+//wbr
 });
